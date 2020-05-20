f7301cfbcb1df889d68052160cde491636135afd
vmwork <49744511+vmwork@users.noreply.github.com>
2020-05-20 20:09:09 +0300

    321

--- js/_custom.js
+++ js/_custom.js
@@ -34,11 +34,91 @@
                 let sltimeOut= setInterval(() => {profitSliderContent2.scrollLeft -= 2 }, 2);  
                  setTimeout(() => {clearInterval(sltimeOut);}, 800);
                 }
+     }
+     
+    
+})();
 
-        
-    }
+(function(){
+    
+// dark style mod
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+
+const headerUl = document.querySelector('header ul');
+const headerContent = header.querySelector('.header-content');
+// button
+const headerContentImgg = header.querySelector('.header-content img');
+// end
+const earnsEveryh2 = document.querySelector('.earns-every h2');
+const earnsEveryP = document.querySelector('.earns-every p');
+
+
+const profitTodayH2 = document.querySelector('.profit-today h2');
+const obviousBefenefitsH2 = document.querySelector('.obvious-befenefits h2');
+const offers = document.querySelector('.offers');
+const footer = document.querySelector('footer');
+// в цикл
+const advantagesDiv = document.querySelectorAll('.advantages div');
+const advantagesP =document.querySelectorAll('.advantages p');
+const headerLi = document.querySelectorAll('header li');
+const profitTodayDiv = document.querySelectorAll('.profit-today div');
+const obviousBefenefitsSpan = document.querySelectorAll('.obvious-befenefits span');
+const obviousBefenefitsP = document.querySelectorAll('.obvious-befenefits p');
+const offersP = document.querySelectorAll('.offers p');
+const footerA = document.querySelectorAll('footer a');
+const footerLi = document.querySelectorAll('footer li');
+
+
+
+
+
+if(
+  headerContentImgg.addEventListener('click', darkTheme)  
+)
+
+
+function darkTheme (e){
+    e.preventDefault();
+    body.classList.toggle('darkStyle')
+    header.classList.toggle('darkStyle')
    
+    headerUl.classList.toggle('darkStyle')
+    headerContent.classList.toggle('darkStyle')
+    earnsEveryP.classList.toggle('darkStyle')
+    earnsEveryh2.classList.toggle('darkStyle')
+    obviousBefenefitsH2.classList.toggle('darkStyle')
+    profitTodayH2.classList.toggle('darkStyle')
+    offers.classList.toggle('darkStyle')
+    footer.classList.toggle('darkStyle')
+
+    for (let i = 0; i < advantagesDiv.length; i++ ){
+        advantagesDiv[i].classList.toggle('darkStyle-helper')
+    }
+    for (let i = 0; i < advantagesP.length; i++ ){
+        advantagesP[i].classList.toggle('darkStyle-helper')
+    }
+    for (let i = 0; i < profitTodayDiv.length; i++ ){
+        profitTodayDiv[i].classList.toggle('darkStyle')
+    }
+    for (let i = 0; i < obviousBefenefitsSpan.length; i++ ){
+        obviousBefenefitsSpan[i].classList.toggle('darkStyle-helper')
+    }
+    for (let i = 0; i < obviousBefenefitsP.length; i++ ){
+        obviousBefenefitsP[i].classList.toggle('darkStyle')
+    }
+    for (let i = 0; i < offersP.length; i++ ){
+        offersP[i].classList.toggle('darkStyle')
+    }
+    for (let i = 0; i < footerA.length; i++ ){
+        footerA[i].classList.toggle('darkStyle')
+    }
+    for (let i = 0; i < footerLi.length; i++ ){
+        footerLi[i].classList.toggle('darkStyle')
+    }
 
     
-    
+}
+
+
 })();
