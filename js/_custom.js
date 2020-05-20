4a645901bb4d5294aa46cbf56a596be9e3900cb8
vmwork <49744511+vmwork@users.noreply.github.com>
2020-05-20 18:41:39 +0300

    12331

--- js/_custom.js
+++ js/_custom.js
@@ -8,4 +8,37 @@
             mobileMenu.classList.toggle('open-menu')
         })
     }
+    const buttonsProfitSlider = document.querySelectorAll('.profit-today-slider__buttons button');
+    const buttonsProfitSlider2 = document.querySelectorAll('.offers-slider__buttons button');
+    
+    const profitSliderContent = document.querySelector('.profit-today-slider');
+    const profitSliderContent2 = document.querySelector('.offers-slider');
+
+    if (buttonsProfitSlider && buttonsProfitSlider){
+        buttonsProfitSlider[0].addEventListener('click', scrollLeft)
+        buttonsProfitSlider[1].addEventListener('click', scrollright)
+        buttonsProfitSlider2[0].addEventListener('click', scrollLeft2)
+        buttonsProfitSlider2[1].addEventListener('click', scrollright2)
+    
+        function scrollLeft(){
+            let sltimeOut= setInterval(() => {profitSliderContent.scrollLeft += 2 }, 2); 
+             setTimeout(() => {clearInterval(sltimeOut);}, 1170);}
+        function scrollright(){
+            let sltimeOut= setInterval(() => {profitSliderContent.scrollLeft -= 2 }, 2);  
+             setTimeout(() => {clearInterval(sltimeOut);}, 1150);
+            }
+            function scrollLeft2(){
+                let sltimeOut= setInterval(() => {profitSliderContent2.scrollLeft += 2 }, 2); 
+                 setTimeout(() => {clearInterval(sltimeOut);}, 800);}
+            function scrollright2(){
+                let sltimeOut= setInterval(() => {profitSliderContent2.scrollLeft -= 2 }, 2);  
+                 setTimeout(() => {clearInterval(sltimeOut);}, 800);
+                }
+
+        
+    }
+   
+
+    
+    
 })();
